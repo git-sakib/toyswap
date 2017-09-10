46c42c5de344c8bc94e945b835b3169471c3448d
gitsakib <nsts66@gmail.com>
2017-09-10 23:45:34 +0600

    updates

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            //new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
             new AppBundle\AppBundle(),
             new ToyBundle\ToyBundle(),
         ];
--- src/ToyBundle/Form/ToyType.php
+++ src/ToyBundle/Form/ToyType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+//use RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle;
 
 class ToyType extends AbstractType
 {
@@ -15,13 +18,16 @@ class ToyType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name',TextType::class,array('label' => 'form-control'))
-            ->add('detail')
-            ->add('status')
-            ->add('picture')
-            ->add('type')
-            ->add('date')
-            ->add('userId');
+        $builder
+        ->add('name', TextType::class, array( 'attr' => array( 'class' => 'form-control') ))
+        ->add('detail', TextType::class, array( 'attr' => array( 'class' => 'form-control') ))
+        ->add('status', TextType::class, array( 'attr' => array( 'class' => 'form-control') ))
+        ->add('picture', FileType::class, array( 'attr' => array( 'class' => 'form-control') ))
+        ->add('type', TextType::class, array( 'attr' => array( 'class' => 'form-control') ))
+        ->add('date', DateType::class, array( 'attr' => array( 'class' => 'form-control') ))
+        ->add('userId', TextType::class, array( 'attr' => array( 'class' => 'form-control') ));
+
+        //ladybug_dump($builder); die;
     }
     
     /**
